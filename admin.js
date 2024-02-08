907e187cf519f73938bcb2196bd5e095a2edc226
VedantDevani <140062104+VedantDevani@users.noreply.github.com>
2024-02-08 18:07:59 +0530

    https://github.com/VedantDevani/Node.js-/upload/main
    Handlebar & EJS

--- admin.js
+++ admin.js
@@ -1,20 +1,28 @@
-const express = require('express');
 const path = require('path');
 
+const express = require('express');
+
 const rootDir = require('../util/path');
+
 const router = express.Router();
-const products =[];
 
+const products = [];
+
+// /admin/add-product => GET
 router.get('/add-product', (req, res, next) => {
-    res.sendFile(path.join(rootDir, 'views', 'add-product.html'))
-    //res.send('<form action="/product" method="POST"><input type ="text" name="title"><button type="submit">Add product</button></form>');
+  res.render('add-product', {
+    pageTitle: 'Add Product',
+    path: '/admin/add-product', 
+    formsCSS: true,
+    productCSS: true,
+    activeAddProduct: true });
 });
 
+// /admin/add-product => POST
 router.post('/add-product', (req, res, next) => {
-   products.push({title: req.body.title});
-    res.redirect('/');
+  products.push({ title: req.body.title });
+  res.redirect('/');
 });
-
 
 exports.routes = router;
-exports.products =products;
+exports.products = products;
--- app.js
+++ app.js
@@ -1,25 +1,33 @@
 const path = require('path');
+
 const express = require('express');
 const bodyParser = require('body-parser');
-const app =express()
+const expressHbs = require('express-handlebars');
 
-app.set('view engine','pug');
-app.set('views','views')
+const app = express();
 
+app.engine(
+  'hbs',
+  expressHbs.engine ({
+    layoutsDir: 'views/layouts/',
+    defaultLayout: 'main-layout',
+    extname: 'hbs'
+  })
+);
+app.set('view engine', 'hbs');
+app.set('views', 'views');
 
-const adminData = require ('./Routes/admin');
-const shopRoutes = require('./Routes/shop');
+const adminData = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
-app.use(bodyParser.urlencoded({extended:false}));
-app.use(express.static(path.join(__dirname,'public')));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(adminData.routes);
+app.use('/admin', adminData.routes);
 app.use(shopRoutes);
- 
-app.use ((req,res,next) => {
-    res.status(404).sendFile(path.join(__dirname, 'views','404.html'));
+
+app.use((req, res, next) => {
+  res.status(404).render('404', { pageTitle: 'Page Not Found' });
 });
 
-app.listen(3001);
-
-
+app.listen(3004);
